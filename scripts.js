e8618dce0d6f297522381bce6beb707dc6ef75ba
alicet <alicetsai.x@gmail.com>
2022-07-19 15:20:02 -0400

    flags on select change instead of submit completed

--- scripts.js
+++ scripts.js
@@ -70,7 +70,8 @@ const fxApp = {};
 fxApp.init = () => {
     fxApp.timeOfDay();
     fxApp.populateDropDown();
-    fxApp.getUserInput();
+    fxApp.changeEvent();
+    fxApp.submitEvent();
     fxApp.currencySwitcher();
 };
 
@@ -87,6 +88,8 @@ fxApp.lastAnimatedSpan = document.querySelector('#lastAnimatedSpan');
 fxApp.firstAnimatedSpan = document.querySelector('#firstAnimatedSpan');
 fxApp.sourceSymbolContainer = document.querySelector('.sourceSymbolContainer');
 fxApp.buttonSwitch = document.querySelector('.switch');
+fxApp.selects = document.querySelectorAll('select');
+// console.log(fxApp.selects);
 
 fxApp.currencies = {AED: "United Arab Emirates Dirham", AFN: "Afghan Afghani", ALL: "Albanian Lek", AMD: "Armenian Dram", ANG: "Netherlands Antillean Guilder", AOA: "Angolan Kwanza", ARS: "Argentine Peso", AUD: "Australian Dollar", AWG: "Aruban Florin", AZN: "Azerbaijani Manat", BAM: "Bosnia-Herzegovina Convertible Mark", BBD: "Barbadian Dollar", BDT: "Bangladeshi Taka", BGN: "Bulgarian Lev", BHD: "Bahraini Dinar", BIF: "Burundian Franc", BMD: "Bermudan Dollar", BND: "Brunei Dollar", BOB: "Bolivian Boliviano", BRL: "Brazilian Real", BSD: "Bahamian Dollar", BTC: "Bitcoin", BTN: "Bhutanese Ngultrum", BWP: "Botswanan Pula", BYN: "New Belarusian Ruble", BYR: "Belarusian Ruble", BZD: "Belize Dollar", CAD: "Canadian Dollar", CDF: "Congolese Franc", CHF: "Swiss Franc", CLF: "Chilean Unit of Account (UF)", CLP: "Chilean Peso", CNY: "Chinese Yuan", COP: "Colombian Peso", CRC: "Costa Rican Colón", CUC: "Cuban Convertible Peso", CUP: "Cuban Peso", CVE: "Cape Verdean Escudo", CZK: "Czech Republic Koruna", DJF: "Djiboutian Franc", DKK: "Danish Krone", DOP: "Dominican Peso", DZD: "Algerian Dinar", EGP: "Egyptian Pound", ERN: "Eritrean Nakfa", ETB: "Ethiopian Birr", EUR: "Euro", FJD: "Fijian Dollar", FKP: "Falkland Islands Pound", GBP: "British Pound Sterling", GEL: "Georgian Lari", GGP: "Guernsey Pound", GHS: "Ghanaian Cedi", GIP: "Gibraltar Pound", GMD: "Gambian Dalasi", GNF: "Guinean Franc", GTQ: "Guatemalan Quetzal", GYD: "Guyanaese Dollar", HKD: "Hong Kong Dollar", HNL: "Honduran Lempira", HRK: "Croatian Kuna", HTG: "Haitian Gourde", HUF: "Hungarian Forint", IDR: "Indonesian Rupiah", ILS: "Israeli New Sheqel", IMP: "Manx pound", INR: "Indian Rupee", IQD: "Iraqi Dinar", IRR: "Iranian Rial", ISK: "Icelandic Króna", JEP: "Jersey Pound", JMD: "Jamaican Dollar", JOD: "Jordanian Dinar", JPY: "Japanese Yen", KES: "Kenyan Shilling", KGS: "Kyrgystani Som", KHR: "Cambodian Riel", KMF: "Comorian Franc", KPW: "North Korean Won", KRW: "South Korean Won", KWD: "Kuwaiti Dinar", KYD: "Cayman Islands Dollar", KZT: "Kazakhstani Tenge", LAK: "Laotian Kip", LBP: "Lebanese Pound", LKR: "Sri Lankan Rupee", LRD: "Liberian Dollar", LSL: "Lesotho Loti", LTL: "Lithuanian Litas", LVL: "Latvian Lats", LYD: "Libyan Dinar", MAD: "Moroccan Dirham", MDL: "Moldovan Leu", MGA: "Malagasy Ariary", MKD: "Macedonian Denar", MMK: "Myanma Kyat", MNT: "Mongolian Tugrik", MOP: "Macanese Pataca", MRO: "Mauritanian Ouguiya", MUR: "Mauritian Rupee", MVR: "Maldivian Rufiyaa", MWK: "Malawian Kwacha", MXN: "Mexican Peso", MYR: "Malaysian Ringgit", MZN: "Mozambican Metical", NAD: "Namibian Dollar", NGN: "Nigerian Naira", NIO: "Nicaraguan Córdoba", NOK: "Norwegian Krone", NPR: "Nepalese Rupee", NZD: "New Zealand Dollar", OMR: "Omani Rial", PAB: "Panamanian Balboa", PEN: "Peruvian Nuevo Sol", PGK: "Papua New Guinean Kina", PHP: "Philippine Peso", PKR: "Pakistani Rupee", PLN: "Polish Zloty", PYG: "Paraguayan Guarani", QAR: "Qatari Rial", RON: "Romanian Leu", RSD: "Serbian Dinar", RUB: "Russian Ruble", RWF: "Rwandan Franc", SAR: "Saudi Riyal", SBD: "Solomon Islands Dollar", SCR: "Seychellois Rupee", SDG: "Sudanese Pound", SEK: "Swedish Krona", SGD: "Singapore Dollar", SHP: "Saint Helena Pound", SLL: "Sierra Leonean Leone", SOS: "Somali Shilling", SRD: "Surinamese Dollar", STD: "São Tomé and Príncipe Dobra", SVC: "Salvadoran Colón", SYP: "Syrian Pound", SZL: "Swazi Lilangeni", THB: "Thai Baht", TJS: "Tajikistani Somoni", TMT: "Turkmenistani Manat", TND: "Tunisian Dinar", TOP: "Tongan Paʻanga", TRY: "Turkish Lira", TTD: "Trinidad and Tobago Dollar", TWD: "New Taiwan Dollar", TZS: "Tanzanian Shilling", UAH: "Ukrainian Hryvnia", UGX: "Ugandan Shilling", USD: "United States Dollar", UYU: "Uruguayan Peso", UZS: "Uzbekistan Som", VEF: "Venezuelan Bolívar Fuerte", VND: "Vietnamese Dong", VUV: "Vanuatu Vatu", WST: "Samoan Tala", XAF: "CFA Franc BEAC", XAG: "Silver (troy ounce)", XAU: "Gold (troy ounce)", XCD: "East Caribbean Dollar", XDR: "Special Drawing Rights", XOF: "CFA Franc BCEAO", XPF: "CFP Franc", YER: "Yemeni Rial", ZAR: "South African Rand", ZMK: "Zambian Kwacha (pre-2013)", ZMW: "Zambian Kwacha", ZWL: "Zimbabwean Dollar"};
 
@@ -120,7 +123,7 @@ fxApp.populateDropDown = () => {
 }
 
 
-fxApp.getUserInput = () => { 
+fxApp.submitEvent = () => { 
     fxApp.form.addEventListener('submit', (e) => {
         e.preventDefault();
         
@@ -130,16 +133,14 @@ fxApp.getUserInput = () => {
         fxApp.moneyLoader.classList.remove('money');
         fxApp.moneyLoader.classList.remove('potato');
         
-        fxApp.selectedSourceCurrency = fxApp.sourceCurrency.selectedOptions[0].value;
-        fxApp.selectedTargetCurrency = fxApp.targetCurrency.selectedOptions[0].value;
+        // fxApp.selectedSourceCurrency = fxApp.sourceCurrency.selectedOptions[0].value;
+        // fxApp.selectedTargetCurrency = fxApp.targetCurrency.selectedOptions[0].value;
         fxApp.amountValue =  fxApp.inputAmount.value;
         
         if(fxApp.amountValue <= 0){
             alert("You have no money. Please don't travel")
         } else {
         if(fxApp.selectedSourceCurrency !== fxApp.selectedTargetCurrency){
-            fxApp.getSourceFlag(fxApp.selectedSourceCurrency);
-            fxApp.getTargetFlag(fxApp.selectedTargetCurrency);
             fxApp.getExchangeRate();
         }else{
             alert('Please select different currencies')
@@ -148,6 +149,20 @@ fxApp.getUserInput = () => {
     })
 }
 
+fxApp.changeEvent = () => {
+    fxApp.selects.forEach(dropdown => {
+        dropdown.addEventListener('change', () => {
+            console.log(dropdown.id)
+            fxApp.selectedCurrency = dropdown.selectedOptions[0].value;
+            // console.log(fxApp.selectedCurrency);
+            if (dropdown.id == 'sourceCurrency') {
+                fxApp.getSourceFlag(fxApp.selectedCurrency);
+            } else {
+                fxApp.getTargetFlag(fxApp.selectedCurrency);
+            }
+        })
+    });
+}
 
 fxApp.getExchangeRate = () => {
     fxApp.url = new URL("https://api.apilayer.com/currency_data/live");
@@ -245,6 +260,7 @@ fxApp.getSourceFlag = (currencyCode) => {
     })
         .then(function (results) {
             // console.log(results);
+            fxApp.sourceFlags.innerHTML = '';
             fxApp.sourceSymbol = results[0].currencies[currencyCode].symbol;
             results.forEach(country => {
                 fxApp.flagURL = country.flags.png;
@@ -271,6 +287,7 @@ fxApp.getTargetFlag = (currencyCode) => {
         .then(function (results) {
             // console.log(results);
             // console.log(results[0].currencies[currencyCode].symbol);
+            fxApp.targetFlags.innerHTML = '';
             fxApp.targetSymbol = results[0].currencies[currencyCode].symbol;
             results.forEach(country => {
                 fxApp.flagURL = country.flags.png;
